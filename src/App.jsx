6fec18d615ea2cd0527fcb1e8a925d4abbb27cf1
Ademola <ademolaogunmokun@yahoo.com>
2022-08-22 18:00:10 +0100

    style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 import SearchCoin from './Components/SearchCoin';
 import PriceDisplay from './Components/PriceDisplay';
 import Topfive from './Components/Topfive';
+import { useLayoutEffect } from 'react';
 function App() {
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = '#5b4379';
+  });
 
 
 
--- src/Components/TopfiveExerpt.jsx
+++ src/Components/TopfiveExerpt.jsx
@@ -3,18 +3,20 @@ import { FaArrowsAltH } from 'react-icons/fa';
 
 export default function TopfiveExerpt({ coin }) {
   return (
-    <div>
+    <div className='"border-4 border-rgb(113 113 122) border'>
       <div>
         {coin.name} <span><img style={{
           display: 'inline-block',
         }} className='max-h-5' src={coin.image} alt={coin.name} /></span>
+        <div className= {Math.sign(coin.price_change_percentage_24h) === -1 ? 'text-rose-600' : 'text-green-400' }>
+          {coin.price_change_percentage_24h}</div>
       </div>
       <span>
         <span>{coin.symbol} <FaArrowsAltH style={{
           display: 'inline-block',
         }}/> <span>usd</span></span>
       </span>
-      <div>
+      <div className='text-center font-medium'>
         {coin.current_price}
       </div>
     </div>
