49edfccd1b62eac848f6f2e0f788d3f390f659ea
Ademola <ademolaogunmokun@yahoo.com>
2022-08-23 09:34:51 +0100

    remove style on the button

--- src/Components/Topfive.jsx
+++ src/Components/Topfive.jsx
@@ -14,8 +14,7 @@ export default function Topfive() {
   }
 
   return ( <div>
-    <button onClick={refetch} type="button" className="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none
-    focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Refresh</button>
+    <button onClick={refetch} type="button" >Refresh</button>
     <div className='flex flex-row gap-x-6'>
 
       {fivecoin?.map(coin => <TopfiveExerpt key={coin.id} coin={coin} />)}
